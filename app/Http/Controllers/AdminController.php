1901f24c79fa71e822b0d8fe5f19048a3ea818e1
Yash-2907 <guptayp_1@rknec.edu>
2023-12-24 14:15:05 +0530

    ADMIN CONTROLLER CHANGES

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -11,7 +11,7 @@ class AdminController extends Controller
      */
     public function index()
     {
-        //
+        return view('admin.index');
     }
 
     /**
@@ -60,5 +60,9 @@ class AdminController extends Controller
     public function destroy(string $id)
     {
         //
+    }
+
+    public function __construct() {
+        $this->middleware('auth');
     }
 }
