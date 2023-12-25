62788082be2792140d6fb3367fd28da47ea7f352
Yash-2907 <guptayp_1@rknec.edu>
2023-12-26 01:10:06 +0530

    image part added

---
+++ resources/views/admin/app.blade.php
@@ -0,0 +1,10 @@
+<a
+    href="{{ route('admin-categories') }}"
+    class="block px-4 py-2 hover:bg-indigo-800 rounded-md"
+    >All Categories</a
+>
+<a
+    href="{{ route('admin-categories-create') }}"
+    class="block px-4 py-2 hover:bg-indigo-800 rounded-md"
+    >Create Category</a
+>
